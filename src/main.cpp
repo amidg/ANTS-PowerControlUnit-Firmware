0591f772596ef250a2d90460fa5c72860673e754
Dmitrii Gusev <amidg4x4dima@gmail.com>
2021-07-16 15:03:36 -0700

    PCU fully tested

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,15 @@
 #define SCL 22
 #define SDA 21
 
+bool is12Venabled = 0;
+bool is5VDenabled = 0;
+bool is3V3Denabled = 0;
+bool isCharging = 0;
+
 //PCU FUNCTIONS:
 void startCharging(bool isChargingDetected);
-void enabledRouterSpeakerPower(void);
-void enabled5VdigitalPower(void);
+void enableRouterSpeakerPower();
+void enable5VdigitalPower(void);
 void enable3V3digitalPower(void);
 
 //main functions
@@ -28,20 +33,53 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("start charging");
 
+  //test codes -> uncomment certain function to use is separately
+  enableRouterSpeakerPower();
+  enable5VdigitalPower();
+  enable3V3digitalPower();
+  startCharging(true);
 }
 
 //functions
 void startCharging(bool isChargingDetected) {
   if (isChargingDetected) {
     digitalWrite(STARTCHARGING, HIGH);
+    digitalWrite(ENABLE5VD, LOW);
+    digitalWrite(ENABLE3V3D, LOW);
     Serial.println("charging started");
-    delay(2000);
+    delay(5000);
   } else {
     Serial.println("ERROR: CHARGING NOT DETECTED");
   }
 
   digitalWrite(STARTCHARGING, LOW);
   delay(2000);
+}
+
+void enableRouterSpeakerPower() {
+  digitalWrite(ENABLE12V, HIGH);
+  Serial.println("12V power rail is enabled");
+  delay(1000);
+  //Serial.println("12V power rail is disabled");
+  //digitalWrite(ENABLE12V, LOW);
+  //delay(20000);
+}
+
+void enable5VdigitalPower() {
+  digitalWrite(ENABLE5VD, HIGH);
+  Serial.println("5V digital rail is enabled");
+  delay(1000);
+  //Serial.println("5V digital rail is disabled");
+  //digitalWrite(ENABLE5VD, LOW);
+  //delay(20000);
+}
+
+void enable3V3digitalPower() {
+  digitalWrite(ENABLE3V3D, HIGH);
+  Serial.println("3V3 digital rail is enabled");
+  delay(1000);
+  //Serial.println("3V3 digital rail is disabled");
+  //digitalWrite(ENABLE3V3D, LOW);
+  //delay(20000);
 }
