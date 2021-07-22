68aae0ed59357eb9ace1a059b2b1b8d1375cdf61
Dmitrii Gusev <amidg4x4dima@gmail.com>
2021-07-22 12:57:30 -0700

    added ROS stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "Arduino.h"
 #include "ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Int8.h"
+
+#define IGRNOREDEBUG 1 //must be set to 0 to enable fully working 
 
 #define SYSGOOD 13
 #define ENABLE12V 14 
@@ -15,29 +18,40 @@ bool is12Venabled = 0;
 bool is5VDenabled = 0;
 bool is3V3Denabled = 0;
 bool isCharging = 0;
+bool chargingDetected = 0;
 
 //PCU FUNCTIONS:
-void startCharging(bool isChargingDetected);
+void detectCharging();
 void enableRouterSpeakerPower();
 void enable5VdigitalPower(void);
 void enable3V3digitalPower(void);
 
-//ROS DEFINITIONS: ============================================================================
+//WI-FI DEFINITIONS: ============================================================================
 #define ESP32
 const char* ssid     = "AutoBot1_2G";
 const char* password = "mse2021cap";
 IPAddress ip(192, 168, 1, 3);
 IPAddress server(192,168,100,100);
 const uint16_t serverPort = 11413; //port 1141x, where x =1 for DCU1, 2 for DCU2, and 3 for PCU
-ros::NodeHandle nh;
+
+//ROS DEFINITIONS: ============================================================================
+ros::NodeHandle PCU;
 // Make a chatter publisher
 std_msgs::String str_msg;
-ros::Publisher chatter("pcu", &str_msg);
+ros::Publisher chatter("pcu_test", &str_msg);
 
 // Be polite and say hello
 char hello[13] = "PCU IS HERE";
 uint16_t period = 20;
 uint32_t last_time = 0;
+
+//contactor power
+int contactorEnabled;
+void startChargingByROS(const std_msgs::Int8& msg1);
+
+ros::Subscriber<std_msgs::Int8> ChargingLock("/pcu/chargingunlock", startChargingByROS);
+std_msgs::String chargingStatus_msg;
+ros::Publisher chargingStatusPub("gigavac_feedback", &chargingStatus_msg);
 
 // MAIN FUNCTION ==============================================================================
 void setup() {
@@ -54,8 +68,6 @@ void setup() {
   digitalWrite(ENABLE12V, LOW);
   digitalWrite(STARTCHARGING, LOW);
 
-  delay(10000);
-
   //ROS PART --------------------------------------------------------------------------------------------------
   WiFi.begin(ssid, password);
 
@@ -70,84 +82,88 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   // Set the connection to rosserial socket server
-  nh.getHardware()->setConnection(server, serverPort);
-  nh.initNode();
+  PCU.getHardware()->setConnection(server, serverPort);
+  PCU.initNode();
 
   // Another way to get IP
   Serial.print("IP = ");
-  Serial.println(nh.getHardware()->getLocalIP());
+  Serial.println(PCU.getHardware()->getLocalIP());
+
+  //SUBSCRIBER - PUBLISHER SECTION
+  PCU.subscribe(ChargingLock); //charging lock
+  PCU.advertise(chargingStatusPub); //charging status publisher
 
   // Start to be polite
-  nh.advertise(chatter);
+  PCU.advertise(chatter);
 }
 
 // LOOP FUNCTION ==========================================================================
 void loop() {
   // put your main code here, to run repeatedly:
-
-  //test codes -> uncomment certain function to use is separately
-  //enableRouterSpeakerPower();
-  //enable5VdigitalPower();
-  //enable3V3digitalPower();
-  //digitalWrite(SYSGOOD, HIGH);
-  //startCharging(true);
-
-  if(millis() - last_time >= period)
-  {
-    last_time = millis();
-    if (nh.connected())
-    {
-      Serial.println("Connected");
-      // Say hello
-      str_msg.data = hello;
-      chatter.publish( &str_msg );
-    } else {
-      Serial.println("Not Connected");
-    }
+  //first of all check DCU connection to ROS -> do not start program if no ROS node
+  while (!PCU.connected() && !IGRNOREDEBUG) {
+    Serial.println("ERROR: NO ROS CONNECTION");
   }
-  nh.spinOnce();
+  
+  //Automatically enable all the secondary voltage rails
+  enableRouterSpeakerPower(); //must be automatic
+  enable5VdigitalPower(); //must be automatic
+  enable3V3digitalPower(); //must be automatic
+
+  if (PCU.connected() && is12Venabled && is5VDenabled && is3V3Denabled) {
+    digitalWrite(SYSGOOD, HIGH); //indicates that system correctly peforms, BLUE LED
+  }
+
+  //publish charging state
+  chargingStatusPub.publish( &chargingStatus_msg ); //power locker, str_msg.data is generated based on the input from the ROS power locker
+
+  //publish battery voltage and battery current
+
+
+  PCU.spinOnce();
   delay(1);
 }
 
-//functions
-void startCharging(bool isChargingDetected) {
-  if (isChargingDetected) {
-    digitalWrite(STARTCHARGING, HIGH);
+// FUNCTIONS =====================================================================================
+void startChargingByROS(const std_msgs::Int8& msg1) {
+  //function that controls charging by ROS
+  if (msg1.data == 1 && chargingDetected) {
     digitalWrite(ENABLE5VD, LOW);
     digitalWrite(ENABLE3V3D, LOW);
-    Serial.println("charging started");
-    delay(5000);
+    digitalWrite(STARTCHARGING, HIGH);
+    chargingStatus_msg.data = "Charging...";
   } else {
-    Serial.println("ERROR: CHARGING NOT DETECTED");
+    digitalWrite(ENABLE5VD, HIGH);
+    digitalWrite(ENABLE3V3D, HIGH);
+    digitalWrite(STARTCHARGING, LOW);
   }
+}
 
-  digitalWrite(STARTCHARGING, LOW);
-  delay(2000);
+void detectCharging() {
+  if (digitalRead(DETECTCHARGING)) {
+    chargingDetected = 1;
+  } else {
+    Serial.println("CHARGING NOT DETECTED");
+  }
 }
 
 void enableRouterSpeakerPower() {
   digitalWrite(ENABLE12V, HIGH);
   Serial.println("12V power rail is enabled");
   delay(1000);
-  //Serial.println("12V power rail is disabled");
-  //digitalWrite(ENABLE12V, LOW);
-  //delay(20000);
+  is12Venabled = 1;
 }
 
 void enable5VdigitalPower() {
   digitalWrite(ENABLE5VD, HIGH);
   Serial.println("5V digital rail is enabled");
   delay(1000);
-  //Serial.println("5V digital rail is disabled");
-  //digitalWrite(ENABLE5VD, LOW);
-  //delay(20000);
+  is5VDenabled = 1;
 }
 
 void enable3V3digitalPower() {
   digitalWrite(ENABLE3V3D, HIGH);
   Serial.println("3V3 digital rail is enabled");
   delay(1000);
-  //Serial.println("3V3 digital rail is disabled");
-  //digitalWrite(ENABLE3V3D, LOW);
-  //delay(20000);
+  is3V3Denabled = 1;
 }
